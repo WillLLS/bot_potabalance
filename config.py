9b6e8bbf0966821ca1e12e5ed357fc2a7a5aa09b
William <lalis.william@gmail.com>
2024-04-19 19:37:32 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+path_authorization = os.path.join("authorization", "authorization.json")
+path_balance = os.path.join("data", "balance.json")
+path_usernames = os.path.join("users", "usernames.json")
---
+++ main.py
@@ -0,0 +1,33 @@
+import logging
+import json
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
+
+import time
+
+from utils.command import start, balance, spent, sent, hist, button, help
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+    
+
+if __name__ == '__main__':
+    
+    application = ApplicationBuilder().token("").build()
+    
+    application.add_handler(CallbackQueryHandler(button))
+    
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('balance', balance))
+    application.add_handler(CommandHandler('spent', spent))
+    application.add_handler(CommandHandler('sent', sent))
+    application.add_handler(CommandHandler('hist', hist))
+    application.add_handler(CommandHandler('help', help))
+
+    
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
